586ad9033b13ff76d170f4f7c846745ee9bf65b4
Mr_Wang <318448121@qq.com>
2020-03-02 17:25:46 +0800

    wblog commit

---
+++ src/components/index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+      <el-container>
+        <el-header style="background: rgba(105,105,105,0)">
+          <el-row>
+            <el-col :span="6"><div class="grid-content bg-purple">汪仔码头的博客</div></el-col>
+            <el-col :span="18"><div class="grid-content bg-purple-light">
+              <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                <el-menu-item index="1">处理中心</el-menu-item>
+                <el-submenu index="2">
+                  <template slot="title">我的工作台</template>
+                  <el-menu-item index="2-1">选项1</el-menu-item>
+                  <el-menu-item index="2-2">选项2</el-menu-item>
+                  <el-menu-item index="2-3">选项3</el-menu-item>
+                  <el-submenu index="2-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-menu-item index="3" disabled>消息中心</el-menu-item>
+                <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+              </el-menu>
+              <div class="line"></div>
+            </div></el-col>
+          </el-row>
+        </el-header>
+
+        <el-container style="height: 1000px;">
+          <el-aside width="400px" style="background: #0a0d69">
+            <el-row>
+              <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+            </el-row>
+
+          </el-aside>
+
+          <el-main style="background: #426925">
+            <div class="block">
+              <el-carousel height="150px">
+                <el-carousel-item v-for="item in 4" :key="item">
+                  <h3 class="small">{{ item }}</h3>
+                </el-carousel-item>
+              </el-carousel>
+            </div>
+            <div>
+              <template>
+                <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
+                  <li v-for="i in 10" class="infinite-list-item">{{ i }}</li>
+                </ul>
+              </template>
+
+            </div>
+
+          </el-main>
+        </el-container>
+
+        <el-footer style="background: #69141c">Footer</el-footer>
+      </el-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      //name: 'HelloWorld',
+      name: 'Index',
+     // component: HelloWorld
+      component: Index
+    }
+  ]
+})
